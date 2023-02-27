25b8e17fddef2daf49f3baec8cc63a23a4a662b3
Idan <mquinhoz@hotmail.com>
2023-02-27 08:59:29 -0300

    feat: name validation

--- src/middlewares/checkBodyReq.middleware.ts
+++ src/middlewares/checkBodyReq.middleware.ts
@@ -1,11 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodTypeAny } from "zod";
+import { AppDataSource } from "../data-source";
+import Movie from "../entities/movie.entity";
+import { AppError } from "../errors";
 
 const checkBodyRequest =
   (schema: ZodTypeAny) =>
-  (req: Request, res: Response, next: NextFunction): void => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const check = schema.parse(req.body);
     req.body = check;
+
+    if (req.method === "POST") {
+      const movieRepository = AppDataSource.getRepository(Movie);
+      const findMovie = await movieRepository.findOne({
+        where: {
+          name: req.body.name,
+        },
+      });
+      if (findMovie) {
+        throw new AppError("Movie already exists", 409);
+      }
+    }
 
     return next();
   };
